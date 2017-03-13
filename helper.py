35957725a8c3f6ccf1a151df29a097182cb97a17
Hussein ElBendak <helbendak@me.com>
2017-03-13 20:39:36 +0000

    Added helper functions.

---
+++ helper.py
@@ -0,0 +1,26 @@
+def fast(x,e,m):
+    X=x
+    E = e
+    Y = 1
+    while E > 0:
+        if E % 2 == 0:
+            X = (X * X) % m
+            E = E/2
+        else:
+            Y = (X * Y) % m
+            E = E-1
+            return Y
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
