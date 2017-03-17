b7b9f218b4bb544f76236a4007a0b48b5db32e0d
Hussein ElBendak <helbendak@me.com>
2017-03-17 18:05:27 +0000

    Clean up code.

--- code.py
+++ code.py
@@ -1,22 +1,23 @@
-with open("code.param.txt","r") as f: #Opens code.param file and stores its text into data variable
-    data = f.readlines()
+def read_file():
+    with open("code.param.txt", "r") as f:  # Opens code.param file and stores its text into data variable
+        data = f.readlines()
 
-main_list = []
-for line in data: #Splits the code.param file line by line and appends each line into main_list array
-    words = line.split()
-    main_list.append(words)
+    main_list = []
+    for line in data:  # Splits the code.param file line by line and appends each line into main_list array
+        words = line.split()
+        main_list.append(words)
 
-#Splits the main_array into different arrays and then stores each value in its appropriate variable and type
-numbers_list = main_list[0]
-p = int(numbers_list[0])
-q = int(numbers_list[1])
-e = int(numbers_list[2])
-text1 = main_list[1]
-text1=text1[0]
-text2 = main_list[2]
-text2=text2[0]
+    # Splits the main_array into different arrays and then stores each value in its appropriate variable and type
+    numbers_list = main_list[0]
+    p = int(numbers_list[0])
+    q = int(numbers_list[1])
+    e = int(numbers_list[2])
+    text1 = main_list[1]
+    text1 = text1[0]
+    text2 = main_list[2]
+    text2 = text2[0]
+    print p, q, e
+    print text1
+    print text2
+    return p, q, e, text1, text2
 
-
-print p,q,e
-print text1
-print text2
