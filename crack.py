53aa8b0ea89ab9b804fb0695c40b17bd42560bbc
Zheng He Hu <aeonzh@hotmail.com>
2017-03-18 11:36:48 +0000

    More comments

--- crack.py
+++ crack.py
@@ -1,18 +1,7 @@
 import helper
 
-#def read_file(madrigal_code):
-#    with open(madrigal_code, "r") as f:  # Opens code.param file and stores its text into data variable
-#        data = f.readlines()
-#
-#    main_list = []
-#    for line in data:  # Splits the code.param file line by line and appends each line into main_list array
-#        words = line.split()
-#        main_list.append(words)
-#    return main_list
-
-# TODO : Add comments for this file
-
-def prime_factorization(n):
+def prime_factorization(n): 
+# Brute-force prime factorization for n
 	i = 2
 	factors = []
 	while i * i <= n:
@@ -26,9 +15,8 @@ def prime_factorization(n):
 	return factors
 
 if __name__ == '__main__':
-	n = 32193613398841823
-	e = 17
-	#main_list = read_file("madrigal.code.txt")
+	n = 32193613398841823 	# Public key n
+	e = 17					# Public key e
 	factors = prime_factorization(n)
 	p = factors[0]
 	q = factors[1]
@@ -37,4 +25,4 @@ if __name__ == '__main__':
 	phi = (p-1)*(q-1)
 	print "phi = %d" % phi
 	d = helper.modinv(e, phi)
-	print "d = %d" % d
+	print "d = %d" % d 		# Private key d
